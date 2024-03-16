081563685e9448fbf522708a499e856043d1a014
Samyar <samyarkd@Gmail.com>
2024-03-16 13:29:46 +0330

    init: mvp link parser bot

--- src/app/tut/[tutId]/quiz/page.tsx
+++ src/app/tut/[tutId]/quiz/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import Link from "next/link";
+import { motion } from "framer-motion";
 import { useState } from "react";
 import { Heading } from "~/components/ui/Typography";
+import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
-import { Card, CardContent } from "~/components/ui/card";
-import { cn } from "~/lib/utils";
-
+import { Card, CardContent, CardHeader } from "~/components/ui/card";
+import { wait } from "~/lib/utils";
 const quizQuestions = [
   {
     question: 'What does "koko" mean?',
@@ -81,6 +81,22 @@ const quizQuestions = [
 
 const TutorialPage = ({ params }: { params: { tutId: string } }) => {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [answerList, setAnswerList] = useState<
+    { answer: string | null; question: string; isCorrect: boolean | null }[]
+  >([]);
+  const [currentAnswer, setCurrentAnswer] = useState<string | null>(null);
+
+  async function addAnswer(
+    answer: string,
+    question: string,
+    isCorrect: boolean,
+  ) {
+    setCurrentAnswer(answer);
+    await wait(800);
+    setCurrentAnswer(null);
+    setAnswerList([...answerList, { answer, question, isCorrect }]);
+    changeActiveIndex(1);
+  }
 
   function changeActiveIndex(next: -1 | 1) {
     if (activeIndex === 0) {
@@ -97,65 +113,101 @@ const TutorialPage = ({ params }: { params: { tutId: string } }) => {
   }
 
   return (
-    <div className="grid gap-6 justify-center items-center content-center justify-items-center min-h-screen">
-      <Card className="w-96 max-w-2xl shadow-md backdrop-blur bg-white/35 text-center">
-        <CardContent className="p-2">
-          <Heading>Step By Step Tutorial</Heading>
-        </CardContent>
-      </Card>
-      <div className="relative h-52 w-full">
-        {quizQuestions.map((step, idx) => {
-          return (
-            <Card
-              key={idx}
-              style={{
-                right: activeIndex === idx ? 0 : idx * -5,
-                bottom: activeIndex === idx ? 0 : idx * -1,
-                transform: activeIndex === idx ? "" : `rotate(${idx * 1}deg)`,
-                zIndex: -idx + (activeIndex === idx ? 100 : 0),
-              }}
-              className="w-full absolute min-h-56 backdrop-blur bg-white/35 text-center"
-            >
-              <CardContent className="pt-6 text-start whitespace-pre-line">
-                {step.question}
-              </CardContent>
-            </Card>
-          );
-        })}
+    <div className="grid gap-14 justify-center items-center content-center justify-items-center min-h-screen">
+      <div className="relative">
+        <Card className="w-96 max-w-2xl z-10 shadow-md relative backdrop-blur bg-white/35 text-center">
+          <CardContent className="p-2">
+            <Heading>10 Step Quiz</Heading>
+          </CardContent>
+        </Card>
+        <div className="grid w-full gap-2 grid-cols-10 absolute -top-1/3 left-0 px-2 justify-items-center">
+          {answerList
+            .filter((q) => q?.isCorrect !== null)
+            .map((step) => {
+              return (
+                <motion.div
+                  key={step.question}
+                  initial={{ top: 0, opacity: 0.5 }}
+                  animate={{ top: "-10%", opacity: 1 }}
+                  className="relative"
+                >
+                  <Badge
+                    variant={step.isCorrect ? "success" : "destructive"}
+                    className="w-5 h-10"
+                  />
+                </motion.div>
+              );
+            })}
+        </div>
       </div>
-      <div className="grid grid-cols-4 w-full gap-x-4 gap-y-2">
-        <Button
-          disabled={activeIndex === 0}
-          onClick={() => {
-            changeActiveIndex(-1);
-          }}
-          className={cn("col-span-1 shadow-md disabled:cursor-not-allowed")}
-          variant="secondary"
-        >
-          Back
-        </Button>
-        <Button
-          disabled={activeIndex === quizQuestions.length - 1}
-          onClick={() => {
-            changeActiveIndex(1);
-          }}
-          className="col-span-3 shadow-md disabled:cursor-not-allowed"
-        >
-          Next
-        </Button>
-        <Link className="col-span-4" href={`/tut/${params.tutId}/quiz`}>
-          <Button
-            variant={
-              activeIndex === quizQuestions.length - 1 ? "default" : "link"
-            }
-            className="w-full"
-          >
-            {activeIndex === quizQuestions.length - 1
-              ? "Take a Quiz"
-              : "Skip To take a Quiz"}
-          </Button>
-        </Link>
-      </div>
+      {answerList.length !== quizQuestions.length && (
+        <div className="relative h-52 w-full">
+          {quizQuestions.map((step, questionIdx) => {
+            return (
+              <Card
+                className="w-full absolute min-h-56 backdrop-blur bg-white/35 text-center"
+                key={questionIdx}
+                style={{
+                  zIndex:
+                    -questionIdx + (activeIndex === questionIdx ? 100 : 0),
+                  right: activeIndex === questionIdx ? 0 : questionIdx * -5,
+                  bottom: activeIndex === questionIdx ? 0 : questionIdx * -1,
+                  transform:
+                    activeIndex === questionIdx
+                      ? ""
+                      : `rotate(${questionIdx * 1}deg)`,
+                }}
+              >
+                <CardHeader>{step.question}</CardHeader>
+                <CardContent className="pt-6 grid grid-cols-2 gap-2 text-start whitespace-pre-line">
+                  {step.answers.map((an, idx) => {
+                    return (
+                      <Button
+                        disabled={
+                          currentAnswer !== null
+                            ? currentAnswer === an
+                              ? false
+                              : true
+                            : false
+                        }
+                        variant={
+                          currentAnswer
+                            ? currentAnswer === an
+                              ? currentAnswer === step.correctAnswer
+                                ? "success"
+                                : "destructive"
+                              : "secondary"
+                            : "secondary"
+                        }
+                        onClick={async () => {
+                          if (currentAnswer) return;
+
+                          await addAnswer(
+                            an,
+                            step.question,
+                            an === step.correctAnswer,
+                          );
+                        }}
+                        key={idx}
+                      >
+                        {an}
+                      </Button>
+                    );
+                  })}
+                </CardContent>
+              </Card>
+            );
+          })}
+        </div>
+      )}
+      {answerList.length === quizQuestions.length && (
+        <div className="flex flex-col gap-2">
+          <Card className="w-full shadow-md">
+            <CardHeader>Well-done 🎉 now let's review your answers</CardHeader>
+          </Card>
+          <Button className="shadow-md">Review Answers</Button>
+        </div>
+      )}
     </div>
   );
 };
