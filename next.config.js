5220863ed11962334938c8ee1bdc90588bef0a4b
Samyar <samyarkd@Gmail.com>
2024-05-20 11:22:34 +0330

    fix: exclude dbschema from eslint

--- next.config.js
+++ next.config.js
@@ -5,10 +5,6 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-};
+const config = {};
 
 export default config;
